fb603eff206b0256b3b804865ee9704b3425f3a6
Sai Bharadwaj <saibharadwaj.k3@gmail.com>
2020-12-18 08:45:23 +0530

    weather pro final

---
+++ public/Weather.js
@@ -0,0 +1,387 @@
+document.getElementById("title").scrollIntoView();
+
+const wDay = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+const wMonth = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apl",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+];
+const iconValue = {
+    CLEARSKY: "clear sky",
+    FEWCLOUDS: "few clouds",
+    SCATTEREDCLOUDS: "scattered clouds",
+    BROKENCLOUDS: "broken clouds",
+    SHOWERRAIN: "shower rain",
+    RAIN: "rain",
+    THUNDERSTORM: "thunderstorm",
+    SNOW: "snow",
+    MIST: "mist",
+};
+
+// fetching from local rout
+// place info
+
+async function fetchCurrentPlace(latitude, longitude) {
+    const url1 = `/fetchPlaceWeather/${latitude}/${longitude}`;
+    //fetch
+    let response = await fetch(url1);
+    // console.log(response);
+    let obj = await response.json();
+    // console.log(obj);
+    let data = obj.place_info;
+    console.log(data);
+
+    //call openweather function to get weather info
+    fetchOpenWeather(obj);
+    //call draw function to draw graph
+
+    draw(obj);
+
+    //pulling date and time
+    let dt = new Date(data.dt * 1000);
+    let forecastDate = `${wDay[dt.getDay()]} ${
+    wMonth[dt.getMonth()]
+  } ${dt.getDate()}`;
+
+    //pulling current temperature
+
+    let temperature = data.main.temp - 273.15;
+
+    //populating dom
+
+    document.getElementById(
+        "location"
+    ).innerHTML = `(<i class="fas fa-location-arrow"></i>): ${data.name},${data.sys.country}`;
+
+    // document.getElementById(
+    //     "summary"
+    // ).innerHTML = `(<i class="fas fa-cloud"></i>): ${data.weather[0].main}`;
+    document.getElementById(
+        "dayTime"
+    ).innerHTML = `(<i class="far fa-clock"></i>): ${forecastDate}
+            `;
+    document.getElementById("currentTemp").innerHTML = ` ${temperature.toFixed(
+    2
+  )}${"°C"}`;
+
+    document.getElementById(
+        "Iconsummary"
+    ).innerHTML = `${data.weather[0].main}<br>${data.weather[0].description}`;
+
+    let bgUrl = bgChange(`${data.weather[0].description}`);
+
+    let showcase = document.querySelector(".showcase");
+    showcase.style.background = `url(${bgUrl})`;
+    showcase.style.backgroundRepeat = "no-repeat";
+    showcase.style.backgroundSize = "cover";
+    showcase.style.backgroundPosition = "center center";
+}
+//weather info
+function fetchOpenWeather(info) {
+    console.log(info);
+    let data = info.weather_info;
+    //initilising rrquired data
+    let icon = data.current.weather[0].icon;
+    let humidity = data.current.humidity;
+    let windSpeed = data.current.wind_speed;
+    let ts1 = new Date(data.current.sunrise * 1000);
+    let ts2 = new Date(data.current.sunset * 1000);
+    // console.log(ts1, ts2);
+    let sunRise_h = ts1.getHours();
+    let sunRise_m = ts1.getMinutes();
+    let SunSet_h = ts2.getHours();
+    let SunSet_m = ts2.getMinutes();
+
+    //populating dom
+    document.getElementById(
+        "Sun_Rise"
+    ).innerHTML = `Sunrise(<i class="fas fa-sun"></i>): ${sunRise_h}.${sunRise_m} A.M`;
+    document.getElementById(
+        "Sun_set"
+    ).innerHTML = `Sunset(<i class="fas fa-cloud-sun"></i>): ${
+    SunSet_h - 12
+  }.${SunSet_m} p.M`;
+
+    document.getElementById(
+        "Humidity"
+    ).innerHTML = `Humidity(<i class="fas fa-tint"></i>): ${Math.round(
+    humidity
+  )}%`;
+    document.getElementById(
+        "wind"
+    ).innerHTML = `WindSpeed(<i class="fas fa-wind"></i>): ${windSpeed}mp/h`;
+    //  console.log(icon);
+    document.getElementById("WeatherIcon").src = getIcon(icon);
+
+    //render the forecasts tabs daily and hourly
+    document.getElementById("dailyForecast").innerHTML = renderDailyForecast(
+        data.daily
+    );
+    document.getElementById("weeklyForecast").innerHTML = renderHourlyForecast(
+        data.hourly
+    );
+
+    //calling function to get icon
+    getIcon(icon);
+
+    document.querySelector(".loader").style.display = "none";
+
+    showAlert(
+        "Weather forecast based on address,to which your wi-fi is registered to (Not GPS).For accurate results open site in mobile",
+        "success"
+    );
+}
+
+//bg chang
+function bgChange(discription) {
+    switch (discription) {
+        case iconValue.CLEARSKY:
+            return "images/clearsky.webp";
+
+        case iconValue.FEWCLOUDS:
+            return "images/fewclouds.webp";
+
+        case iconValue.SCATTEREDCLOUDS:
+            return "images/scatteredclouds.jpg";
+
+        case iconValue.BROKENCLOUDS:
+            return "images/scatteredclouds.jpg";
+        case iconValue.SHOWERRAIN:
+            return "images/showerrain.jpg";
+
+        case iconValue.RAIN:
+            return "images/rain.jpg";
+
+        case iconValue.THUNDERSTORM:
+            return "images/thunderstorm.webp";
+
+        case iconValue.SNOW:
+            return "images/snow.jpg";
+
+        case iconValue.MIST:
+            return "images/mist.webp";
+
+        default:
+            return "images/clearsky.webp";
+    }
+}
+//icon
+function getIcon(icon) {
+    //for morning icon
+    if (icon.indexOf("d") > -1) {
+        return `http://openweathermap.org/img/wn/${icon}@4x.png`;
+    }
+    //for night icon
+    if (icon.indexOf("n") > -1) {
+        return `http://openweathermap.org/img/wn/${icon}@4x.png`;
+    } else {
+        return "images/sun.jpg";
+    }
+}
+//render the hourly forecast
+function renderHourlyForecast(fcData) {
+    // console.log(fcData);
+    let resultsHTML =
+        "<tr><th>Time</th><th>Humidity</th><th>Temp</th><th>wind_speed</th></tr>";
+    rowcount = fcData.length;
+    if (rowcount > 8) {
+        rowcount = 8;
+    }
+
+    for (i = 0; i < rowcount; i++) {
+        let ts = new Date(fcData[i].dt * 1000);
+        // console.log(ts);
+        let humidity = "";
+
+        let timeValue;
+
+        //unix time needs to be formatted for display
+        let hours = ts.getHours();
+        let min = ts.getMinutes();
+        // console.log(hours, min);
+        if (hours > 0 && hours <= 12) {
+            timeValue = "" + hours;
+        } else if (hours > 12) {
+            timeValue = "" + (hours - 12);
+        } else if (hours == 0) {
+            timeValue = "12";
+        }
+        timeValue += hours >= 12 ? " PM" : " AM"; // get AM/PM
+
+        humidity = `${fcData[i].humidity}%`;
+
+        let tempHigh = `${Math.round(fcData[i].temp - 273.15)}&deg C`;
+        let windSpeed = `${fcData[i].wind_speed}mp/h`;
+
+        resultsHTML += renderRow(timeValue, humidity, tempHigh, windSpeed);
+    }
+
+    return resultsHTML;
+}
+//render the daily forecast
+function renderDailyForecast(fcData) {
+    // console.log(fcData);
+    let resultsHTML =
+        "<tr><th>Day</th><th>Conditions</th><th>Max_Temp</th><th>Min_temp</th></tr>";
+    rowcount = fcData.length;
+    if (rowcount > 8) {
+        rowcount = 8;
+    }
+
+    for (i = 0; i < rowcount; i++) {
+        let ts = new Date(fcData[i].dt * 1000);
+        let dayTime = `${wDay[ts.getDay()]} ${
+      wMonth[ts.getMonth()]
+    } ${ts.getDate()}`;
+        // let dayTime = wDay[ts.getDay()];
+        let description = fcData[i].weather[0].description;
+        let tempHigh = `${Math.round(fcData[i].temp.max - 273.15)}&deg C`;
+        let tempLow = `${Math.round(fcData[i].temp.min - 273.15)}&deg C`;
+
+        resultsHTML += renderRow(dayTime, description, tempHigh, tempLow);
+    }
+
+    return resultsHTML;
+}
+//template function to render grid colums
+function renderRow(dayTime, des, tempHigh, colVal4) {
+    return `<tr><td>${dayTime}</td><td>${des}</td><td>${tempHigh}</td><td>${colVal4}</td></tr>`;
+}
+
+function draw(object) {
+    array48 = object.weather_info.hourly;
+
+    let xs = [];
+    let ys = [];
+
+    array48.forEach((item) => {
+        let timeValue;
+        let hours = new Date(item.dt * 1000).getHours();
+        if (hours > 0 && hours <= 12) {
+            timeValue = "" + hours;
+        } else if (hours > 12) {
+            timeValue = "" + (hours - 12);
+        } else if (hours == 0) {
+            timeValue = "12";
+        }
+        timeValue += hours >= 12 ? " PM" : " AM";
+        let temperature = `${(item.temp - 273.15).toFixed(2)}`;
+
+        xs.push(timeValue);
+        ys.push(parseFloat(temperature));
+    });
+
+    let ctx = document.getElementById("graph48").getContext("2d");
+
+    console.log(xs, ys);
+
+    let myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: xs,
+            datasets: [{
+                label: "Next 48 hours Temp in °C",
+                data: ys,
+                fill: false,
+
+                backgroundColor: "rgba(255,255,255,1)",
+                borderColor: "rgba(255,0,0,1)",
+                borderWidth: 1,
+            }, ],
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    gridLines: {
+                        display: true,
+                        color: "#FFFFFF",
+                    },
+                    ticks: {
+                        // Include a dollar sign in the ticks
+                        callback: function(value, index, values) {
+                            return value + "°";
+                        },
+                    },
+                }, ],
+
+                xAxes: [{
+                    gridLines: {
+                        display: true,
+                        color: "#FFFFFF",
+                    },
+                }, ],
+            },
+        },
+    });
+}
+
+initGeoLocation();
+
+function initGeoLocation() {
+    if (navigator.geolocation) {
+        //returns a callback function
+        navigator.geolocation.getCurrentPosition(success, fail);
+    } else {
+        showAlert("Look like your browser do not support GeoLocation", "danger");
+    }
+}
+
+//if access location is allowed
+function success(position) {
+    document.querySelector(".center").classList.add("loader");
+    // fetchOpenWeather(position.coords.latitude, position.coords.longitude);
+    fetchCurrentPlace(position.coords.latitude, position.coords.longitude);
+}
+//if access location is denied
+function fail() {
+    //add hard coded lat and long
+    showAlert(
+        "To fetch weather data site need access to your location..!",
+        "danger"
+    );
+    // fetchCurrentPlace(13.3, 78.68);
+}
+
+// dom events
+
+function showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className} mt-3`;
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector(".container");
+    const titleRow = document.querySelector(".row1");
+    container.insertBefore(div, titleRow);
+    container.scrollIntoView();
+
+    //vanish in 3 sec
+
+    setTimeout(() => document.querySelector(".alert").remove(), 9000);
+}
+
+document.querySelector(".b1").addEventListener("click", (e) => {
+    document.getElementById("hourly").classList.add("active");
+    document.getElementById("weekly").classList.remove("active");
+    document.getElementById("graph").classList.remove("active");
+});
+
+document.querySelector(".b2").addEventListener("click", (e) => {
+    document.getElementById("weekly").classList.add("active");
+    document.getElementById("hourly").classList.remove("active");
+    document.getElementById("graph").classList.remove("active");
+});
+
+document.querySelector(".b3").addEventListener("click", (e) => {
+    document.getElementById("graph").classList.add("active");
+    document.getElementById("hourly").classList.remove("active");
+    document.getElementById("weekly").classList.remove("active");
+});
---
+++ script.js
@@ -0,0 +1,28 @@
+const fetch = require("node-fetch");
+require("dotenv").config();
+//create server
+const express = require("express");
+const app = express();
+app.listen(2000, () => console.log("listening at 2000"));
+app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
+//server side fetch
+app.get("/fetchPlaceWeather/:lat/:lng", async(request, response) => {
+    let latlng = request.params;
+    const lat = latlng.lat;
+    const lng = latlng.lng;
+    Api_key = process.env.API_KEY;
+    //fetch place info
+    const place_url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${Api_key}
+        `;
+    const place_res = await fetch(place_url);
+    const place_info = await place_res.json();
+    //fetch weather
+    const weather_url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=minutely,alerts&appid=${Api_key}
+    `;
+    const weather_res = await fetch(weather_url);
+    const weather_info = await weather_res.json();
+    total_data = { place_info, weather_info };
+    response.json(total_data);
+    console.log(total_data);
+});
