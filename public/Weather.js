3f688a1500e91dd417cebb3dca102cae8a36b954
saibharadwajk3 <saibharadwaj.k3@gmail.com>
2022-05-16 00:19:52 +0530

    custom message based on device

--- public/Weather.js
+++ public/Weather.js
@@ -134,10 +134,27 @@ function fetchOpenWeather(info) {
     document.querySelector(".loader").style.display = "none";
     document.querySelector(".mar").style.borderLeft = "1px solid white";
 
-    showAlert(
-        "Weather forecast based on address,to which your wi-fi is registered to (Not GPS).For accurate results open site in mobile",
-        "success"
-    );
+    //check if user uses mobile or desktop
+    /* Storing user's device details in a variable*/
+    let details = navigator.userAgent;
+  
+    /* Creating a regular expression 
+    containing some mobile devices keywords 
+    to search it in details string*/
+    let regexp = /android|iphone|kindle|ipad/i;
+
+    /* Using test() method to search regexp in details
+    it returns boolean value*/
+    let isMobileDevice = regexp.test(details);
+
+    if (!isMobileDevice) {
+        showAlert(
+            "Weather forecast based on address,to which your wi-fi is registered to (Not GPS).For accurate results open site in mobile",
+            "success"
+        );
+        }
+
+    
 }
 
 //bg chang
